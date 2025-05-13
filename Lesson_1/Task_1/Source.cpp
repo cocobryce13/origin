30d6f42c086c6f6b8776d4cd6512b4538525e91a
Sokol <sokol.msi@gmail.com>
2025-05-13 20:25:54 +0500

    Решение Task#1

--- Lesson_1/Task_1/Source.cpp
+++ Lesson_1/Task_1/Source.cpp
@@ -3,13 +3,15 @@
 struct point {
 	double m_x;
 	double m_y;
-	point(double x, double y) {
+	point(double x, double y) 
+	{
 		m_x = x;
 		m_y = y;
 	}
 };
 
-void print_point(const point& point_object) {
+void print_point(const point& point_object) 
+{
 	std::cout << "x:" << point_object.m_x << ", y: "
 		<< point_object.m_y << std::endl;
 }
@@ -18,7 +20,8 @@ void print_point(const point& point_object) {
 int main()
 {
 	int i = 0;
-	for (i = 0; i < 5; i++); {
+	for (i = 0; i < 5; i++)
+	{
 		point my_point(i, 2 * i);
 		print_point(my_point);
 	}
