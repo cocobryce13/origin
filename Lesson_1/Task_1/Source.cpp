1d59b319317d4f8a06570f125827e38958fd31e5
Sokol <sokol.msi@gmail.com>
2025-05-13 19:36:10 +0500

    Добавил папку Task2

--- Lesson_1/Task_1/Source.cpp
+++ Lesson_1/Task_1/Source.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
-using namespace std;
 
 struct point {
 	double m_x;
 	double m_y;
 	point(double x, double y) {
 		m_x = x;
-		y = y;
+		m_y = y;
 	}
-}
+};
 
 void print_point(const point& point_object) {
 	std::cout << "x:" << point_object.m_x << ", y: "
 		<< point_object.m_y << std::endl;
 }
 
-int Main()
+
+int main()
 {
-	int i;
+	int i = 0;
 	for (i = 0; i < 5; i++); {
 		point my_point(i, 2 * i);
 		print_point(my_point);
 	}
 	return 0;
-}
-
-void print_point(const point& point_object) {
-	std::cout << "x:" << point_object.m_x << ", y: "
-		<< point_object.m_y << std::endl;
 }
